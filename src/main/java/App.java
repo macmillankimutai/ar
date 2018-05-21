c3698cc2c1dce34f7f76b5926f7f52315c1de348
macmillan kimutai <mcmyllano@gmail.com>
2018-05-21 08:44:15 +0300

    informative commit message

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,11 +11,11 @@ public class App {
     String layout = "templates/layout.vtl";
 
     ProcessBuilder process = new ProcessBuilder();
-     Interger port;
+     Integer port;
      if(process.environment().get("PORT") != null) {
        port = Integer.parseInt(process.environment().get("PORT"));
      } else {
-        port = 4567
+        port = 4567;
      }
 
      setPort(port);
